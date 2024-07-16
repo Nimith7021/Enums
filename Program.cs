e459443215c4dfdf1a32757d9435c7d27dfb0b6c
Nimith Gangadhar Shetty <Nimith.Shetty@asl.mum.sst>
2024-07-16 19:47:33 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enumeration.Types;
+
+namespace Enumeration
+{
+    internal class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Names of Rainbow colors");
+            foreach (string s in Enum.GetNames(typeof(Rainbow))) {
+
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Values of Rainbow Colors");
+            foreach (int item in Enum.GetValues(typeof(Rainbow)))
+            {
+
+                Console.WriteLine(item); 
+            
+            
+            }
+        }
+    }
+}
+
+    
+
---
+++ Types/Rainbow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration.Types
+{
+    enum Rainbow
+    {
+        violet,
+        indigo,
+        blue,
+        green,
+        yellow,
+        orange,
+        red
+    }
+}
